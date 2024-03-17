96a392ecd96dba2e6cea7a35300802e2aa2e09fb
NullBite <me@nullbite.com>
2024-03-17 02:17:46 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.systems.url = "github:nix-systems/default";
+  outputs = { self, nixpkgs, systems }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+  in {
+    devShells = eachSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          act
+          (python311.withPackages (ps: with ps; [ pyyaml ptpython ]))
+        ];
+      };
+    });
+  };
+}
